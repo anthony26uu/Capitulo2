360ea9f2abaa25eab6c592dc8024ceb86067f7c9
Anthony Javier Santana Polanco <anthony26uu@gmail.com>
2017-01-13 18:30:43 -0400

    Modificacion

--- Ej1/Ej1/Program.cs
+++ Ej1/Ej1/Program.cs
@@ -10,29 +10,28 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            //Variables Auxiliares
+            
             double perimetro = 0, lado = 0, cantidad = 0;
 
-            //Explicacion del ejercicio
+           
             Console.WriteLine("Encontrar el Perimetro  de un Poligono.");
            
 
             Console.Write("Ingrese cantidad de lados del Poligono: ");
             cantidad = Int32.Parse(Console.ReadLine());
 
-            //Pediendo datos
+           
             Console.Write("Ingrese el lados del Poligono: ");
-            //Convierte de texto a tipo entero
-            lado = Int32.Parse(Console.ReadLine());
+           lado = Int32.Parse(Console.ReadLine());
 
 
 
 
 
-            //Realiza la operacion
+            
             perimetro = cantidad * lado;
 
-            //Imprimiendo el resultado
+           
             Console.WriteLine("El Area del Cuadrado: " + perimetro);
 
 
